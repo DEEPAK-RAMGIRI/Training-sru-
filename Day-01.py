12967f4abe19c547781c62d5cd64666637046bef
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-05-14 15:13:32 +0530

    Basics

---
+++ Day-01.py
@@ -0,0 +1,72 @@
+
+
+# #              _                         
+# #   _ __  _ _ (_) _ __   ___   _ _   ___ 
+# #  | '_ \| '_|| || '  \ / -_) | ' \ / _ \
+# #  | .__/|_|  |_||_|_|_|\___| |_||_|\___/
+# #  |_|                                   
+
+
+# # # if prime check it is  > 200 
+# # n = int(input())
+# # flag = False
+# # for i in range(2,int(n**0.5)+1):
+# #     if n % i == 0:
+# #         flag = True
+# #         print(f"not prime {n}")
+# # if not flag:
+# #     if n == 1:
+# #         print("not prime and composite")
+# #     else:
+# #         if n < 200:
+# #             print(f"{n} is less than 200")
+# #         else:
+# #             print(f"{n} is greater than 200")
+            
+            
+
+# #                                       _              _  _            _               __                                   
+# #   _ _  ___  _ __   ___ __ __ ___   __| | _  _  _ __ | |(_) __  __ _ | |_  ___  ___  / _| _ _  ___  _ __    __ _  _ _  _ _ 
+# #  | '_|/ -_)| '  \ / _ \\ V // -_) / _` || || || '_ \| || |/ _|/ _` ||  _|/ -_)(_-< |  _|| '_|/ _ \| '  \  / _` || '_|| '_|
+# #  |_|  \___||_|_|_|\___/ \_/ \___| \__,_| \_,_|| .__/|_||_|\__|\__,_| \__|\___|/__/ |_|  |_|  \___/|_|_|_| \__,_||_|  |_|  
+# #                                               |_|                                                                         
+
+
+# # arr = [8,2,3,4,3,3,4,5,6,7,9,2,4]
+# # # ans = []
+# # sets = set()
+# # for i in arr:
+    
+# #     if i not in sets:
+# #         print(i,end=" ")
+# #         sets.add(i)
+# # # print(ans)
+# # # Time complexcity is O(n)
+# # #space complexcity is O(1)
+        
+        
+        
+
+
+# # n = int(input())
+# # print((n * (n+1) )// 2) # Time Complexcity = O(1)
+
+
+
+# lst = [2,3,4,3,2,5,5]
+# #length = odd
+# sums = 0
+# for i in lst:
+#     sums^=i
+# print(sums)
+# #Time complexcity o(n)
+
+
+#     for i in set(lst):
+#         if lst.count(i) == 1:
+#             print(i)
+#             break    
+#     #Time Complexcity O(n^2)
+#     #Space Complexcity O(1)
+
+
