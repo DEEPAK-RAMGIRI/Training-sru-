79d790fb986db2426e15a27ab95db74c507a7b86
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-05-15 08:08:28 +0530

    Recursion

--- Day-03.py
+++ Day-03.py
@@ -46,28 +46,78 @@
 # a = [13,17,21,23,22,7,29]
 # print(loop(a,0))
 
-def trueorfalse(n,count = 0):
+# def trueorfalse(n,count = 0):
+#     if n == 1:
+#         return True
+#     elif n <= 0:
+#         return False 
+#     return trueorfalse(n-3) or trueorfalse(n-5)
+
+
+# Counting minimum no of subtraction to get 1
+# def trueorfalse1(n, count=0):
+#     if n == 1:
+#         return count
+#     elif n <= 0:
+#         return 99999 
+#     return min(trueorfalse1(n - 3, count + 1), trueorfalse1(n - 5, count + 1))
+
+# n = 4
+# ans = trueorfalse1(n)
+# if ans == 99999:
+#     print(False)
+# else:
+#     print(ans)
+
+
+
+
+#Using BFS method to find the ans
+def trueorfalse(n):
     if n == 1:
         return True
     elif n <= 0:
         return False 
+    
     return trueorfalse(n-3) or trueorfalse(n-5)
+    
+# n = 20
+# queue = deque()
+# while True:
+    
+# trueorfalse(n,queue)
 
-
-# Counting minimum no of subtraction to get 1
-def trueorfalse1(n, count=0):
+n = 20
+res = []
+def function(n,temp):
     if n == 1:
-        return count
-    elif n <= 0:
-        return 99999 
-    return min(trueorfalse1(n - 3, count + 1), trueorfalse1(n - 5, count + 1))
+        res.append(temp)
+        return 
+    if n < 1:
+        return 
+    function(n-3,temp + [n-3]) 
+    function(n-5,temp + [n-5])
+    
+function(n,[])
+# print(res)
 
-n = 4
-ans = trueorfalse1(n)
-if ans == 99999:
-    print(False)
-else:
-    print(ans)
+
+# using loops
+res = []
+for i in range(n//3 +1):
+    for j in range(n//5 + 1):
+        if 3*i + 5 *j == n-1:
+            path = []
+            current = n
+            path_steps = [3]*i + [5]*j
+            for step in path_steps:
+                current -= step
+                path.append(current)
+            res.append(path)
+    
+            
+print(res)
+            
 
     
 
