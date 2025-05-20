e70e49068c75d5dfdd172d1f09094701d3d75151
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-05-20 09:40:04 +0530

    binary search alogrithms start

---
+++ Day-08.py
@@ -0,0 +1,16 @@
+# find the floor value of no
+n = 54
+left = 0
+right = n
+while left <=  right:
+    mid = left + ((right - left) >> 1)
+    square = mid * mid 
+    if mid == square:
+        print(mid)
+        break
+    elif square <= n:
+        left = mid + 1
+    else:
+        right = mid - 1
+        
+print(left-1)
