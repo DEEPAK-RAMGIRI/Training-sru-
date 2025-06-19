6645fe73026b440cda0ece0d6864a83edc20c5e4
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-06-19 11:16:42 +0530

    linkedlist

--- new_d4.py
+++ new_d4.py
@@ -38,7 +38,8 @@ class Linkedlist:
             temp = temp.next
         self.printing(temp)
     
-    
+    #Time Complexcity O(n) + O(n//2) ~ O(n)
+    #Space Complexcity O(1)
     def find_middle2(self):
         count = 0 
         temp = self.head
@@ -61,11 +62,100 @@ class Linkedlist:
             fast = fast.next.next
         return slow.data
     
+    #Check Palindrome Linked List
+    #Time Complexcity O(n) + O(n) ~  O(n)
+    #Space Complexcity O(n)
+    def check_palindrome1(self):
+        temp = self.head
+        arr = []
+        while temp:
+            arr.append(temp.data)
+            temp = temp.next
+        return arr == arr[::-1]
     
+    #Time Complexcity  O(n) +O(n//2) + O(n//2) ~ O(n)
+    #Space Complecity O(1)
+    def check_palindrome2(self):
+        temp = self.head
+        
+        def reverse(head):
+            prev,temp = None,head
+            while temp:
+                nextnode,temp.next = temp.next,prev
+                prev,temp = temp,nextnode
+            return prev
+        
+        fast,slow = self.head,self.head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        secound_half = reverse(slow)
+        temp = self.head
+        self.printing(secound_half)
+        while temp != slow:
+            if temp.data != secound_half.data:
+                print(False)
+                break
+            temp = temp.next
+            secound_half = secound_half.next
+        else: print(True)
+    
+    
+   # Odd Even Linked List
+   #Time Complexcity O(n) + O(n) ~ O(n)
+   #Space Complexcity O(n) + O(n)  ~ O(n)
+    def oddEven_Linkedlist1(self):
+        odd,even = [],[]
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1 
+            if count & 1: odd.append(temp.data)
+            else:even.append(temp.data)
+            temp = temp.next
+        odd.extend(even)
+        
+        
+        dummy = Node(0)
+        temp = dummy
+        for i in odd:
+            temp.next = Node(i)
+            temp = temp.next
+        self.printing(dummy.next)
+        
+        
+    def oddEven_Linkedlist2(self):
+        slow,fast = self.head,self.head.next
+        even = fast
+        while fast and fast.next:
+            slow.next = fast.next
+            slow = slow.next
+            fast.next = slow.next
+            fast = fast.next
+        slow.next = even
+        
+        self.printing(self.head)
+            
+
 ll = Linkedlist()
-arr = [1,2,3,4,5,6]
-for i in arr: ll.insert(i) 
+# arr = [1,2,3,4,5,6]
+# for i in arr: ll.insert(i)
+# Middle of the Linked List 
 # ll.find_middle1()
-ll.find_middle2() 
+# ll.find_middle2() 
 #ll.find_middle3()
             
+            
+#Check Palindrome Linked List
+# arr = [1,2,2,1]
+# for i in arr: ll.insert(i)
+# ll.check_palindrome1()
+# ll.check_palindrome2()
+
+
+# Odd Even Linked List
+arr = [1,2,3,4,5]
+for i in arr: ll.insert(i)
+# ll.oddEven_Linkedlist1()
+ll.oddEven_Linkedlist2()
