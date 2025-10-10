e07d66c6f6bd8f38a5b74f9dac99fec9408567b5
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-10-10 23:26:17 +0530

    lcs practiced

--- striver-75.py
+++ striver-75.py
@@ -546,12 +546,48 @@ def lcs(i,j):
     return dp[i][j]
 
 
-print(lcs(len(text1)-1,len(text2)-1))
 # print(dp)
-            
-    
-
         
+# Time Complexcity O(n ^ 2)
+#Space Complexcity O( n ^ 2)    
+dp = dict()
+def lcs(i,j):
+    if i< 0 or j < 0 : return 0
+    if text1[i] == text2[j]: return 1 + lcs(i-1,j-1)
+    elif (i,j) in dp: return dp[i,j]
+    dp[i,j] = max(lcs(i-1,j),lcs(i,j-1))
+    return dp[i,j]
+
+#Tabulation
+# Time Complexcity O(n ^ 2)
+# Space Complexcity O( n ^ 2)
+dp = [[-1 for _ in range(len(text2))] for _ in range(len(text1))]
+
+for i in range(len(text1)):
+    for j in range(len(text2)):
+        if text1[i] == text2[j]:
+            if i-1 >= 0 and j - 1 >= 0:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else: dp[i][j] = 1
+        else:
+            dp[i][j] = max(dp[i - 1][j] if i - 1 >= 0 else 0 , dp[i][j-1] if j-1 >= 0 else 0)
+
+# Space Optimization
+# Time Complexcity O(n^2)
+#Space COmplexcity O(n)
+dp = [-1] * len(text2)
+for i in range(len(text1)):
+    temp = [0] * len(text2)
+    for j in range(len(text2)):
+        if text1[i] == text2[j]:
+            if i-1 >= 0 and j-1 >= 0:
+                temp[j] = 1 + dp[j-1]
+            else:
+                temp[j] = 1
+        else:
+            temp[j] = max(dp[j] if i-1 >= 0 else 0 , temp[j-1] if j-1 >=0 else 0)
+    dp = temp
+# ==========================================    
     
     
   
