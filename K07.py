4267a80c0a00b78edc533de8856f51f89b4aec19
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-11-26 11:53:38 +0530

    Sliding window

--- K07.py
+++ K07.py
@@ -92,10 +92,83 @@ arr = [
 # print(arr)
 
 # 90 deg rotation
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        arr[i][j],arr[j][i] = arr[j][i],arr[i][j]
-    arr[i] = arr[i][::-1]
-print(arr)
+# for i in range(len(arr)):
+#     for j in range(i+1,len(arr)):
+#         arr[i][j],arr[j][i] = arr[j][i],arr[i][j]
+#     arr[i] = arr[i][::-1]
+# print(arr)
+
+arr = [
+[1, 2, 3, 4],
+[5,6,7,8],
+[9,0,1,4],
+[5,2,3,9]
+]
+
+left = 0
+right = len(arr) - 1
+
+#used to pointer approach
+# swapping arr[left] first values with the arr[right] last values
+ 
+# while left < right:
+#     for i in range(len(arr[0])):
+#         arr[left][i],arr[right][~i] = arr[right][~i],arr[left][i]
+#     left+=1
+#     right-=1
+# print(arr)
+
+
+# anagram
+s1 =  'students'
+s2 =  'destntsu'
+
+s1 = 'teams'
+s2 = 'meat'
+
+s1= 'apple'
+s2 = 'leepa'
+
+def anagram():
+    seen = dict()
+    for i in s1:
+        seen[i] = seen.get(i,0)+1
+        
+    for j in s2:
+        if j not in seen:
+            return False
+        seen[j] -= 1
+        if seen[j] == 0:
+            del seen[j]
+    return not seen
+
+# print(anagram())
+
+
+# string of alphabets and digits
+string = 'asw2edqsc4wdvrm21aawenejrg3e'
+
+digits = 2
+left = 0
+maxi = 0
+for right in range(len(string)):
+    
+    if string[right].isnumeric():
+        digits-=1
+    while digits < 0:
+        if string[left].isnumeric():
+            digits+=1
+        left+=1
+    maxi = max(maxi,right - left + 1)
+
+print(maxi)
+        
+    
+        
+        
+        
+    
+
+    
 
 
